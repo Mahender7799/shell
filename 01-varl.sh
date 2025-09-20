8f5c35d7e28a18fc369a5bb81789be1dae5bef96
Mahender7799 <ramavathmahendar76@gmail.com>
2025-09-20 11:18:54 +0530

    demo

---
+++ 01-varl.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+P1=Mahi
+P2=Hima
+
+echo "Hello ${P2}, How are you doing?"
+echo "Hi ${P1}, I'm doing greate how about you?"
+
+
+PERSON1=$1
+PERSON2=$2
+
+echo "Hi $PERSON1"
+echo "Hello $PERSON2, How are you"
