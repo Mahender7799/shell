648a0d2ad0832fc7e7113061456766572e57ecb0
Mahender7799 <65751060+Mahender7799@users.noreply.github.com>
2025-09-20 18:04:25 +0530

    Update special-var.sh

--- special-var.sh
+++ special-var.sh
@@ -2,3 +2,10 @@
 
 echo "All the variable passed to the script : $@"
 echo "All the variable passed to the script : $*"
+echo "script name: $0"
+echo "current directory : $PWD"
+echo "Who is running this: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of this script: $$"
+sleep 50 &
+echo "PID of the last command in backgound is : $!"
