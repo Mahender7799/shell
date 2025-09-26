1b198b7d85b8804e315918e3cf265b7083650069
Mahender7799 <ramavathmahendar76@gmail.com>
2025-09-26 19:32:34 +0530

    addi

--- install.sh
+++ install.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ];then
    echo "Error::Please run this script with root privelege"
+   exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ];then
    echo "Error: Installing MYSQL is failed"
+   exit 1
 else
    echo "Installing MYSQL is success"
 fi   
