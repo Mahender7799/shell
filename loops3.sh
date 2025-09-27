506f211f83c019025282a9a89d7529ecd4e783f8
Mahender7799 <ramavathmahendar76@gmail.com>
2025-09-27 20:23:08 +0530

    finctions

---
+++ loops3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell"
+USERID=$(id -u)
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+LOGG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started at : $(date)"
+echo "Script started executing at: $date"
+if [ $USERID -ne 0 ];then
+   echo -e " ERROR::Please run script with root prevelages " | tee -a $LOGG_FILE
+   exit 1
+fi
+
+VALIDATE(){ ## Functions receives inputs through args just like shell scripts
+    if [ $? -ne 0 ];then
+        echo -e " Installing $2 ... is $R FAILED $N" | tee -a $LOGG_FILE
+        exit 1
+    else
+        echo -e " installing $2 ... is $G SUCCESS $N" | tee -a $LOGG_FILE
+    fi
+}
+
+## $@
+
+for package in $@;do
+   echo "print the pachage name $package"
+done   
