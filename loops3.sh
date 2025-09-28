7ccfa5b6822bd6d304d86163ebc76df66724dbb7
Mahender7799 <ramavathmahendar76@gmail.com>
2025-09-28 14:27:59 +0530

    finctions

--- loops3.sh
+++ loops3.sh
@@ -30,17 +30,13 @@ VALIDATE(){ ## Functions receives inputs through args just like shell scripts
 ## $@
 
 for package in $@
-do 
-   #check package is already installed or not
-   dnf list installed $package &>>LOGG_FILE
-   
-   #if exit status 0, already installed elase not installed
-   if [ $? -ne 0 ];then
-       dnf install $package -y &>>LOGG_FILE
-       VALIDATE $? "$package"
-   else
-      echo -e "$package is alreasy installed ... $Y SKIPPING$N"
-
-   fi
-
-done   
+do
+##check if package is already installed or not
+   dnf list installed $package -y &>>$LOG_FILE
+   if [$? -ne 0];then
+	    dnf install $package -y & >>$LOG_FILE
+        VALIDATE $? "$package"
+	else
+      echo -e "$package is already installed ... $Y SKIPPING$N "
+    fi	 
+done     
