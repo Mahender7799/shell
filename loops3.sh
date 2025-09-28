3fc2bc09ee95c79d68db1b9229a1191157d14e0e
Mahender7799 <ramavathmahendar76@gmail.com>
2025-09-28 14:35:10 +0530

    finctions

--- loops3.sh
+++ loops3.sh
@@ -8,22 +8,22 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell"
 USERID=$(id -u)
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
-LOGG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 echo "Script started at : $(date)"
 echo "Script started executing at: $date"
 if [ $USERID -ne 0 ];then
-   echo -e " ERROR::Please run script with root prevelages " | tee -a $LOGG_FILE
+   echo -e " ERROR::Please run script with root prevelages " | tee -a $LOG_FILE
    exit 1
 fi
 
 VALIDATE(){ ## Functions receives inputs through args just like shell scripts
     if [ $? -ne 0 ];then
-        echo -e " Installing $2 ... is $R FAILED $N" | tee -a $LOGG_FILE
+        echo -e " Installing $2 ... is $R FAILED $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e " installing $2 ... is $G SUCCESS $N" | tee -a $LOGG_FILE
+        echo -e " installing $2 ... is $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
